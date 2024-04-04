e306ba7f982d237c62ef470cf582a10ab5780004
JorgeBlaze <160815006+JorgeBlaze@users.noreply.github.com>
2024-04-04 14:35:55 -0400

    Add files via upload

---
+++ Lab6/Data/IData.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab6.Data
+{
+    public interface IData
+    {
+
+        static List<Book> books = new List<Book>();
+        static List<User> users = new List<User>();
+        static Dictionary<User, List<Book>> borrowedBooks = new Dictionary<User, List<Book>>();
+
+
+    }
+}
